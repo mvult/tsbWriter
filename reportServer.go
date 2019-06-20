665e1005f8dc687d037f3fbc1f4dd957d1189ec7
mvult <lavalle.miguel@gmail.com>
2019-06-20 10:54:23 -0500

    named

--- reportServer.go
+++ reportServer.go
@@ -190,6 +190,17 @@ func getIndex() *template.Template {
 		if err == nil {
 			return t
 		}
+
+		path = filepath.Join(c, "src/github.com/mvult/tsbWriter/index.html")
+		if err != nil {
+			continue
+		}
+		logger.Println(path)
+		t, err = template.ParseFiles(path)
+
+		if err == nil {
+			return t
+		}
 	}
 	path, _ = filepath.Abs("index.html")
 	t, err = template.ParseFiles(path)
